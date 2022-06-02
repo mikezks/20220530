738da4cec0da4a5a245287f348cb83dae9c54931
mikezks <23002661+mikezks@users.noreply.github.com>
2022-06-02 10:17:38 +0200

    feat: reactice design typeahead

--- nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,5 +1,8 @@
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, share, Subscription, tap, timer } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-lib';
+import { debounceTime, delay, distinctUntilChanged, EMPTY, filter, Observable, share, Subscription, switchAll, switchMap, tap, timer } from 'rxjs';
 import { RxConnectorService } from '../../shared/rx-connector/rx-connector.service';
 
 @Component({
@@ -15,14 +18,63 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   );
   subscription = new Subscription();
 
-  constructor(private rx: RxConnectorService) {}
+  control = new FormControl();
+  flights$: Observable<Flight[]> = this.getFlightsStream$();
+  loading = false;
+
+  constructor(
+    private rx: RxConnectorService,
+    private http: HttpClient) {}
 
   ngOnInit(): void {
     // this.rxjsDemo();
 
-    this.rx.connect(timer(0, 1_000), {
+    /* this.rx.connect(timer(0, 1_000), {
       next: console.log
-    });
+    }); */
+  }
+
+  getFlightsStream$(): Observable<Flight[]> {
+    /**
+     * Stream 1: Input field value changes
+     * - Trigger
+     * - Data Provider
+     */
+    return this.control.valueChanges.pipe(
+      // Add additional state
+      // withLatestFrom()
+      // Filtering START
+      filter(city => city.length > 2),
+      debounceTime(300),
+      distinctUntilChanged(),
+      // Filtering END
+      // Side-effect: Loading state -> property
+      tap(() => this.loading = true),
+      /**
+       * Stream 2: Http Backend call -> returns Flight array
+       * - Switch to another stream
+       * - Data Provider -> final state to be rendered out
+       */
+      switchMap(city => this.load(city)),
+      // delay(3_000),
+      // Side-effect: Loading state -> property
+      // Transformation
+      // map()
+      tap(() => this.loading = false)
+    );
+  }
+
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
+
   }
 
   rxjsDemo(): void {
